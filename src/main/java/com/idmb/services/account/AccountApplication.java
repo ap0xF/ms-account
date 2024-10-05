91aba9f2f31ebb2d63b83f70b89c2363ce417111
aadit <aadit.pant@8squarei.com>
2024-10-05 20:24:49 +0545

    first commit

---
+++ src/main/java/com/idmb/services/account/AccountApplication.java
@@ -0,0 +1,14 @@
+package com.idmb.services.account;
+import org.idmb.lib.*;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountApplication.class, args);
+	}
+
+}
